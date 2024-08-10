5334c36633d130a9a9de1095a11fc9986cea8286
Ian <52181020+functionals@users.noreply.github.com>
2024-08-09 22:09:19 -0500

    Source that compiles with clean cross-reference
    There is one line of code out of place and I can't for the life of me find it.  use the following line to suppress the error at runtime:
    :- discontiguous l:sentence/2

--- eeg.pl
+++ eeg.pl
@@ -1,7 +1,12 @@
-l:sentence-->(l:noun_p,l:verb_p);((l:noun_p),(l:prep_p),(l:word));((l:verb),(l:noun_p),(l:prep_p),(l:word)).
+l:sentence-->(l:noun_p,l:verb_p);((l:noun_p),(l:prep_p),l:word);((l:verb),(l:noun_p),(l:prep_p),(l:word)).
 l:sentence:-((idea);(question;command)),((l:noun_p),l:prep_p,l:verb_p).
 l:sentence:-read(49).
+l:sentence:-objective(_).
+l:sentence:-(l:noun_p,l:verb_p);((l:noun_p),(l:prep_p),(l:word));((l:verb),(l:noun_p),(l:prep_p),(l:word)).
 l:sentence(Number)-->l:noun_p(Number),l:verb_p(Number).
+l:sentence(Wordlist,String):-getsentence(Wordlist),objective(String|Wordlist).
+l:sentence(Wordlist,String):-l:grab(l:sentence,l:word(Wordlist,String,objective)).
+l:sentence(Wordlist,String):-l:word(Wordlist,input,String).
 l:word(Char|((Char,String);Rest))-->l:letter(Char|String,Char),l:grab_l((Char|Rest,Rest),(Char|String,String)),form_w(Char|String,String).
 l:verb_p-->l:verb,l:noun_p.
 l:verb_p(Number)-->l:verb(Number),l:noun_p(Number).
@@ -23,7 +28,9 @@ l:verb_p(Number):-l:noun_p(Number).
 l:noun_p(Number):-l:verb_p(Number).
 l:determiner(X,Y,Z):-write(X;Y;Z).
 l:verb(X,Y,Z):-write(X;Y;Z).
+l:verb:-l:verb(_,_,_).
 l:word(X,Y):-l:letter(Y|X,Y).
+l:word:-l:word(input,getletters).
 l:letter(Y,X,Z,P):-l:grab_l(Y|X,X);l:grab_l(X|Z,Z);l:grab_l(Z|P,P).
 form_w(Char|String,String)-->l:word(Char|String,String),l:sentence(String).
 idea:-information;question;command.
@@ -35,6 +42,7 @@ objective(X):-input(X=task).
 input(Wordlist):-getsentence(Wordlist).
 input(P):-(P:Q),display(Q).
 input(_):-assert((_)).
+input(getsentence):-l:sentence(input,objective).
 getsentence(Wordlist):-get0(Char),getrest(Char,Wordlist).
 getrest(46,[]):-!.
 getrest(32,Wordlist):-!,getsentence(Wordlist).
@@ -43,7 +51,6 @@ getletters(46,[],46):-!.
 getletters(32,[],32):-!.
 getletters(Let,[Let|Letters],Nextchar):-get0(Char),getletters(Char,Letters,Nextchar).
 :-op(1200,xfy,(-:-)).
-
 options:-write('Your Choice is either 1 or 2, enter 1 for sentence forms and 2 to stream input in english'),nl,options_display(49),options_choose(49),nl.
 options_display(49):-sentence.
 options_display(49):-get(49),nl.
@@ -57,7 +64,6 @@ copy_list([]-:-[]).
 copy_list([X|Y]-:-[X|Z]):-copy_list(Y-:-Z),tell([a]).
 display(options):-start.
 start:-(options->options_display(49)).
-
 :-dynamic l:grab/2.
 :-dynamic l:letter/2.
 :-dynamic l:noun_p/0.
@@ -65,4 +71,3 @@ start:-(options->options_display(49)).
 :-dynamic l:verb_p/0.
 :-dynamic l:word/4.
 :-dynamic l:grab_l/2.
-
